5815d0c9e3b8beb7da0827dbaf9cd500202bed3f
LUCAS GABRIEL DA SILVA DUARTE <20241011110026@ifrn.local>
2025-01-24 11:56:16 -0300

    time

--- reactiontest.py
+++ reactiontest.py
@@ -26,18 +26,22 @@ while running:
     screen.fill((196, 48, 61)) # VERMELHO
     draw_text("espera ai irmão", text_font, (255, 255, 255), 220, 150)
     pygame.display.flip()
-    
-    time.sleep(random.randrange(1, 5))
+    time.sleep(random.randrange(3, 5))
 
     screen.fill((0, 255, 0))
     pygame.display.flip()
+    start = time.time()
+    
     # screen.fill((95, 217, 100)) # verde 
     while not pygame.mouse.get_pressed()[0]:
       pass  # Wait for the user to click
 
     if pygame.mouse.get_pressed()[0]:
+      end = time.time()
+      length = end - start
+      pygame.display.flip()
       screen.fill((68, 134, 208)) # azul
-      draw_text(f"ygildsasdasdaigyasdiyugfasdyguauyg", text_font, (255, 255, 255), 220, 150)
+      draw_text(f"{length}", text_font, (255, 255, 255), 220, 150)
       pygame.display.flip()
 
   clock.tick(60)  # Pausa e indica a taxa de quadros por segundo (FPS)
